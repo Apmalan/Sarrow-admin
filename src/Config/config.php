244d71809ef6415c5c852f0fd590b62a03739f97
Alan Monte <53954290+Apmalan@users.noreply.github.com>
2019-11-29 21:46:25 -0300

    Conexão de banco de dados aula2

--- src/Config/config.php
+++ src/Config/config.php
@@ -3,4 +3,10 @@
 date_default_timezone_set ( 'America/Sao_Paulo');
 setlocale(LC_TIME,'pt-BR','pt_BR.utf-8','portuguese');
 
+//Arquivos padrões
+require_once (realpath(dirname(__FILE__).'/database.php'));
+
+//Configurações de pastas
+define('MODEL_PATH',realpath(dirname(__FILE__,2). '/models'));
+
 ?>
